02c2c8433518ed34217e25087e3ae42d942c5706
Aman Mittal <amandeepmittal@live.com>
2020-03-05 20:08:02 +0530

    fix: the icon issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,18 +63,20 @@ module.exports = {
         background_color: config.backgroundColor,
         theme_color: config.themeColor,
         display: 'standalone',
-        icons: [
-          {
-            src: '/android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
+        icon: `src/images/android-chrome-192x192.png` // This path is relative to the root of the site.
+
+        // icons: [
+        //   {
+        //     src: '/android-chrome-192x192.png',
+        //     sizes: '192x192',
+        //     type: 'image/png'
+        //   },
+        //   {
+        //     src: '/android-chrome-512x512.png',
+        //     sizes: '512x512',
+        //     type: 'image/png'
+        //   }
+        // ]
       }
     },
     {
