1c3a2254792ebcb86a7710da3f724ede4bfa9b44
Aman Mittal <amandeepmittal@live.com>
2020-06-01 11:13:17 +0530

    fix link

--- src/components/BlogPostFooter.js
+++ src/components/BlogPostFooter.js
@@ -12,7 +12,7 @@ export default function BlogPostFooter() {
             <img className='avatar' src={avatar} alt='Aman Mittal' />
           </div>
           <div>
-            <h3>Author</h3>
+            <h3>👋</h3>
             <p>
               I'm <strong style={{ color: 'white' }}>Aman</strong>
               {` working as an independent fullstack developer with technologies such as Node.js, Reactjs, and React Native. I try to document and write tutorials to help JavaScript Web and Mobile developers.
@@ -38,7 +38,7 @@ export default function BlogPostFooter() {
               <a
                 className='kofi-button'
                 style={{ marginLeft: '10px', backgroundColor: '#EF6753' }}
-                href='https://ko-fi.com/amanhimself'
+                href='https://www.patreon.com/amanhimself'
                 target='_blank'
                 rel='noopener noreferrer'
               >
