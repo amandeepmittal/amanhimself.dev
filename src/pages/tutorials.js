67d9555715b22a231491f07bde34abda75873f0f
Aman Mittal <amandeepmittal@live.com>
2020-06-04 17:50:20 +0530

    fix

--- src/pages/tutorials.js
+++ src/pages/tutorials.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Helmet from 'react-helmet';
 import { graphql, Link } from 'gatsby';
 import Layout from '../components/Layout';
