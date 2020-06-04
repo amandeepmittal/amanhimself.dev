fbe5a921c3cdb82ea8e24a8000b49b418fb6030a
Aman Mittal <amandeepmittal@live.com>
2020-06-04 17:50:50 +0530

    add new blog link page to that work as a redirect to blog

---
+++ src/pages/blog.js
@@ -0,0 +1,113 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+import { graphql, Link } from 'gatsby';
+import Layout from '../components/Layout';
+import PostListing from '../components/PostListing';
+import config from '../data/site-config';
+
+export default class BlogPage extends React.Component {
+  state = {
+    searchTerm: '',
+    posts: this.props.data.posts.edges,
+    filteredPosts: this.props.data.posts.edges
+  };
+
+  handleChange = event => {
+    const { name, value } = event.target;
+
+    this.setState({ [name]: value }, () => this.filterPosts());
+  };
+
+  filterPosts = () => {
+    const { posts, searchTerm } = this.state;
+
+    const filteredPosts = posts.filter(post =>
+      post.node.frontmatter.title
+        .toLowerCase()
+        .includes(searchTerm.toLowerCase())
+    );
+
+    this.setState({ filteredPosts });
+  };
+
+  render() {
+    const { filteredPosts, searchTerm } = this.state;
+    const filterCount = filteredPosts.length;
+    const categories = this.props.data.categories.group;
+
+    return (
+      <Layout>
+        <Helmet title={`Articles – ${config.siteTitle}`} />
+        <div className='container'>
+          <h1 className='articles-title'>Articles</h1>
+          <div className='category-container'>
+            {categories.map(category => {
+              return (
+                <Link
+                  to={`/categories/${category.fieldValue.toLowerCase()}`}
+                  className='category-filter'
+                  key={category.fieldValue}
+                >
+                  {category.fieldValue}
+                </Link>
+              );
+            })}
+          </div>
+          <div className='search-container'>
+            <input
+              className='search'
+              type='text'
+              name='searchTerm'
+              value={searchTerm}
+              placeholder='Type here to search for a post...'
+              onChange={this.handleChange}
+            />
+            <div className='filter-count'>{filterCount}</div>
+          </div>
+          <PostListing postEdges={filteredPosts} />
+        </div>
+      </Layout>
+    );
+  }
+}
+
+export const pageQuery = graphql`
+  query BlogPageQuery {
+    posts: allMarkdownRemark(
+      limit: 2000
+      sort: { fields: [fields___date], order: DESC }
+      filter: { frontmatter: { template: { eq: "post" } } }
+    ) {
+      edges {
+        node {
+          fields {
+            slug
+            date
+          }
+          excerpt(pruneLength: 180)
+          timeToRead
+          frontmatter {
+            title
+            tags
+            categories
+            thumbnail {
+              childImageSharp {
+                fixed(width: 150, height: 150) {
+                  ...GatsbyImageSharpFixed
+                }
+              }
+            }
+            date
+            template
+          }
+        }
+      }
+    }
+    categories: allMarkdownRemark(limit: 2000) {
+      group(field: frontmatter___categories) {
+        fieldValue
+        totalCount
+      }
+    }
+  }
+`;
