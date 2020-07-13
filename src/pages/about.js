e3afbe8cc71320244c483fecddb39ee1767fa293
Aman Mittal <amandeepmittal@live.com>
2020-07-14 00:29:35 +0530

    fix: path to Layout component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import Layout from '../components/layout';
+import Layout from '../components/Layout';
 import SEO from '../components/seo';
 
 import {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 import { graphql, Link } from 'gatsby';
-import Layout from '../components/Layout';
 
+import Layout from '../components/Layout';
 import PostListing from '../components/PostListing';
 import config from '../data/site-config';
 import HeaderIntro from '../components/HeaderIntro';
