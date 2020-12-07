82b4e078ca3f9df5ea1898aedc6abd5c343cda45
amandeepmittal <amandeepmittal@live.com>
2020-12-07 19:18:09 +0530

    Update NewsletterButton.js

--- components/ui/NewsletterButton.js
+++ components/ui/NewsletterButton.js
@@ -7,7 +7,7 @@ const NewsletterButton = () => {
     <div class="mx-auto">
       <button
         type="button"
-        className="text-sm border border-purple-500 bg-purple-500 text-white rounded-md px-4 py-2 transition duration-500 ease select-none hover:bg-purple-400 hover:border-purple-400 focus:outline-none focus:shadow-outline">
+        className="text-base border border-purple-500 bg-purple-500 text-white rounded-md px-4 py-2 transition duration-500 ease select-none hover:bg-purple-400 hover:border-purple-400 focus:outline-none focus:shadow-outline">
         <a href={BlogConfig.newsletter} target="_blank" rel="noreferrer">
           &#128140;&nbsp; Subscribe Newsletter
         </a>
