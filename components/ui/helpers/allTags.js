b0eb44e836a61a4732bdfd6a1a37cac9db1b6eb6
amandeepmittal <amandeepmittal@live.com>
2020-12-08 02:03:57 +0530

    feat: tags

--- components/ui/helpers/allTags.js
+++ components/ui/helpers/allTags.js
@@ -1,70 +1,10 @@
 const slugify = (str) => str.replace(/\s+/g, '-').toLowerCase();
 
-const tags = [
+const tags = [  
   {
-    name: "next.js",
-    slug: slugify('next.js'),
-    bgColor: "bg-yellow-500",
-    fontColor: "text-gray-100",
-  },
-  {
-    name: "node",
-    slug: slugify('node'),
-    bgColor: "bg-green-600",
-    fontColor: "text-gray-100",
-  },
-  {
-    name: "netlify",
-    slug: slugify('netlify'),
-    bgColor: "bg-green-400",
-    fontColor: "text-gray-100",
-  },
-  {
-    name: "firebase",
-    slug: slugify('firebase'),
-    bgColor: "bg-yellow-400",
-    fontColor: "text-gray-900",
-  },
-  {
-    name: "writing",
-    slug: slugify('writing'),
-    bgColor: "bg-yellow-200",
-    fontColor: "text-gray-700",
-  },
-  {
-    name: "javascript",
-    slug: slugify('javascript'),
-    bgColor: "bg-yellow-200",
-    fontColor: "text-gray-700",
-  },  
-  {
-    name: "tools",
-    slug: slugify('tools'),
-    bgColor: "bg-yellow-300",
-    fontColor: "text-gray-800",
-  },
-  {
-    name: "react",
-    slug: slugify('react'),
-    bgColor: "bg-blue-300",
-    fontColor: "text-gray-800",
-  },
-  {
-    name: "gatsby",
-    slug: slugify('gatsby'),
-    bgColor: "bg-purple-700",
-    fontColor: "text-gray-200",
-  },
-  {
-    name: "tailwindcss",
-    slug: slugify('tailwind'),
-    bgColor: "bg-green-400",
-    fontColor: "text-gray-200",
-  },
-  {
-    name: "css",
-    slug: slugify('css'),
-    bgColor: "bg-purple-600",
+    name: "expo",
+    slug: slugify('expo'),
+    bgColor: "bg-gray-900",
     fontColor: "text-gray-200",
   },
   {
@@ -74,11 +14,77 @@ const tags = [
     fontColor: "text-gray-800",
   },
   {
-    name: "expo",
-    slug: slugify('expo'),
-    bgColor: "bg-gray-900",
+    name: "firebase",
+    slug: slugify('firebase'),
+    bgColor: "bg-yellow-400",
+    fontColor: "text-gray-900",
+  },
+  // {
+  //   name: "next.js",
+  //   slug: slugify('next.js'),
+  //   bgColor: "bg-yellow-500",
+  //   fontColor: "text-gray-100",
+  // },
+  {
+    name: "node",
+    slug: slugify('node'),
+    bgColor: "bg-green-600",
+    fontColor: "text-gray-100",
+  },
+  {
+    name: "react",
+    slug: slugify('react'),
+    bgColor: "bg-blue-300",
+    fontColor: "text-gray-800",
+  },
+  {
+    name: "netlify",
+    slug: slugify('netlify'),
+    bgColor: "bg-green-400",
+    fontColor: "text-gray-100",
+  },  
+  {
+    name: "writing",
+    slug: slugify('writing'),
+    bgColor: "bg-yellow-200",
+    fontColor: "text-gray-700",
+  },
+  // {
+  //   name: "javascript",
+  //   slug: slugify('javascript'),
+  //   bgColor: "bg-yellow-200",
+  //   fontColor: "text-gray-700",
+  // },  
+  {
+    name: "tools",
+    slug: slugify('tools'),
+    bgColor: "bg-yellow-300",
+    fontColor: "text-gray-800",
+  },  
+  {
+    name: "gatsby",
+    slug: slugify('gatsby'),
+    bgColor: "bg-purple-700",
     fontColor: "text-gray-200",
   },
+  {
+    name: "graphql",
+    slug: slugify('graphql'),
+    bgColor: "bg-purple-500",
+    fontColor: "text-gray-200",
+  },
+  // {
+  //   name: "tailwindcss",
+  //   slug: slugify('tailwind'),
+  //   bgColor: "bg-green-400",
+  //   fontColor: "text-gray-200",
+  // },
+  // {
+  //   name: "css",
+  //   slug: slugify('css'),
+  //   bgColor: "bg-purple-600",
+  //   fontColor: "text-gray-200",
+  // },  
 ];
 
 export default tags;
--- pages/blog.js
+++ pages/blog.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import { NextSeo } from 'next-seo';
+import classNames from 'classnames';
 
 import {TWHeading} from '../components/ui/Heading';
 import TWBlogCard from '../components/ui/TWBlogCard';
 import TWContainer from '../components/ui/TWContainer';
+import allTags from '../components/ui/helpers/allTags';
+import TWCustomLink from '../components/ui/TWCustomLink';
 
 import { frontMatter as blogPosts } from './blog/**/*.mdx';
 
@@ -36,12 +39,26 @@ const Blog = () => {
         }}
       />
       <TWContainer as="main" noMargin className="md:px-4 space-y-14">
-        <div className="flex flex-col max-w-screen-lg mx-8 items-center justify-center">
+        <div className="flex flex-col max-w-screen-lg mx-8 items-center justify-center">          
           <TWHeading size="h1">Blog Posts - {blogPosts.length}</TWHeading>
           <p className="text-lg text-gray-500 my-4">A collection of articles, tutorials, and writings.</p>
           <input className="bg-white rounded border-0 p-3 focus:outline-none focus:shadow-outline border border-purple-200 rounded-lg py-2 px-4 block w-3/4 appearance-none leading-normal" type="search" onChange={e => setSearchValue(e.target.value)}
           placeholder="Search a post here... " />
-          
+          <div className="flex flex-wrap m-4">
+            {allTags.map((tag) => (
+              <TWCustomLink key={tag.name}
+              scroll={false}
+                            to={`/tag/${tag.slug}`}
+                  className={classNames(
+                  'pointer-events-auto inline-block rounded-lg px-2 py-1 text-xs font-semibold transition-colors duration-200 ease-in-out mx-2 my-2',
+                  tag.bgColor,
+                  tag.fontColor  
+                )}
+                >
+                  #{tag.name}
+                  </TWCustomLink>
+            ))}
+          </div>
         {!filteredBlogPosts.length && 'No posts found.'}
             {filteredBlogPosts.map(frontMatter => (
               <TWBlogCard key={frontMatter.title} {...frontMatter} />
