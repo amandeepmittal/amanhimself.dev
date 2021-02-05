ce3627e922d051b1a7ed1a6b8d5ec2dfe353d200
amandeepmittal <amandeepmittal@live.com>
2021-02-05 13:08:28 +0530

    update sponsor link banner

--- src/components/TWBlogFooterBanner.js
+++ src/components/TWBlogFooterBanner.js
@@ -1,20 +1,18 @@
 import React from 'react';
 
-import banner from '../../content/assets/uidev-banner.jpg';
+import banner from '../../content/assets/dev-apis-banner.png';
 
 const TWBlogFooterBanner = () => {
   return (
     <div className='m-8 md:m-4 flex flex-wrap justify-center items-center'>
-      <div className='bg-white-200 border-2 border-purple-500 relative text-gray-600 py-3 px-3 rounded-lg'>
+      <div className='bg-white-200 border-2 border-purple-500 relative text-gray-600 py-3 px-3 rounded-lg text-center'>
         <p className='font-semibold text-lg'>
-          Want to help support this blog and learn some cool things about
-          JavaScript at the same time?
+          <strong>DEV APIs</strong> is a complete APIs suite for your software
+          development, and business to power-up.
         </p>
-        <p className='text-base py-2'>
-          Click the banner below and sign up to Bytes.
-        </p>
-        <a href='https://ui.dev/bytes/?r=aman' target='_blank'>
-          <img src={banner} width='90%' />
+        <p className='text-base py-2'>Give it a try? Click the banner below.</p>
+        <a href='https://nocodeapi.com/devapis' target='_blank'>
+          <img src={banner} width='100%' />
         </a>
       </div>
       <small className='text-xs text-purple-500'>
