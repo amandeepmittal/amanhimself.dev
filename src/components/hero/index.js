b3d2c0558e3cc735068d95c756609f0dfcc0c1e1
amandeepmittal <amandeepmittal@live.com>
2021-03-14 14:08:31 +0530

    add social icons in <Hero />

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -4,6 +4,7 @@ import { down } from 'styled-breakpoints';
 import { StaticImage } from 'gatsby-plugin-image';
 
 import { config } from '../../helpers';
+import { Twitter, GitHub, Devto, Medium, Instagram } from '../social-icons';
 
 const HeroWrapper = styled.div`
   align-self: center;
@@ -48,6 +49,21 @@ const ContentWrapper = styled.div`
   }
 `;
 
+const SocialIconsWrapper = styled.footer`
+  text-align: center;
+  svg {
+    margin: 0 ${({ theme }) => theme.spacing[2]};
+    height: ${({ theme }) => theme.spacing[6]};
+    &:hover {
+      transform: translateY(-2px);
+      transition: transform 0.35s;
+    }
+  }
+  a {
+    display: inline-block;
+  }
+`;
+
 const Hero = () => {
   return (
     <>
@@ -78,6 +94,48 @@ const Hero = () => {
               @amanhimself
             </a>
           </h2>
+          <SocialIconsWrapper>
+            <a
+              href={config.twitter}
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Twitter Account"
+            >
+              <Twitter />
+            </a>
+            <a
+              href={config.github}
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="GitHub Account"
+            >
+              <GitHub />
+            </a>
+            <a
+              href={config.devto}
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Dev.to Account"
+            >
+              <Devto />
+            </a>
+            <a
+              href={config.medium}
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Medium Account"
+            >
+              <Medium />
+            </a>
+            <a
+              href={config.instagram}
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Instagram Account"
+            >
+              <Instagram />
+            </a>
+          </SocialIconsWrapper>
           <p>
             <span role="img" aria-label="laptop emoji">
               💻{' '}
