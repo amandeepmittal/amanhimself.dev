57b368d3f4ac97fce3fef4742deae224df9d3001
Aman Mittal <amandeepmittal@live.com>
2021-08-28 21:57:33 +0530

    install gatsby plugin to display embed tweets

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     `gatsby-plugin-styled-components`,
+    `gatsby-plugin-twitter`,
     {
       resolve: `gatsby-plugin-web-font-loader`,
       options: {
