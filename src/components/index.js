68d6d3169c86e9159ae7b635d6a3f5d3fa836dbf
Aman Mittal <amandeepmittal@live.com>
2021-09-10 12:40:14 +0530

    migrate uses to now

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
 export { default as Layout } from './layout';
 export { default as Hero } from './hero';
-export { default as Now } from './now';
 export { default as SEO } from './seo';
 export { default as LinkButton } from './link-button';
 export { default as PostInfo } from './post-info';
