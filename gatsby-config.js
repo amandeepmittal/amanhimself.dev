b51dbd22dc0bbc758b9d16be10cf60449c99a884
Aman Mittal <amandeepmittal@live.com>
2021-10-04 01:15:51 +0530

    update theme_color

--- gatsby-config.js
+++ gatsby-config.js
@@ -115,7 +115,7 @@ module.exports = {
         description: 'Software engineer and technical writer.',
         start_url: `/`,
         background_color: `#ffffff`,
-        theme_color: `#c792ea`,
+        theme_color: `#6a5acd`,
         display: `minimal-ui`,
         icon: `src/images/avatar.jpg` // This path is relative to the root of the site.
       }
