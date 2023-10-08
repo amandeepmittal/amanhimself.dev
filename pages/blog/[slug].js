67cbbb62688cbf252b05c5185027cd7edb09b202
Aman Mittal <amandeepmittal@live.com>
2023-10-08 15:46:44 +0530

    Clean up and update package-lock file

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import { MDXRemote } from 'next-mdx-remote';
 import { useRouter } from 'next/router';
 import { serialize } from 'next-mdx-remote/serialize';
