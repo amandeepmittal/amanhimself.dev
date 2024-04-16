c914597f870dbaaca8b26e71ecc9f744c21a6f7c
Aman Mittal <amandeepmittal@live.com>
2024-04-16 15:29:49 +0530

    Update project list

--- src/data/projects.js
+++ src/data/projects.js
@@ -9,19 +9,12 @@ export const ProjectsList = [
   {
     id: '2',
     title: 'Expo Firebase Starter',
-    description: `🔥 A quicker way to start with Expo + Firebase JS projects. Based on latest Expo SDK, provides. Part of expo-community.`,
+    description: `🔥 A quicker way to start with Expo and Firebase JS SDK projects. Based on latest Expo SDK. Part of expo-community.`,
     href: 'https://github.com/expo-community/expo-firebase-starter',
     expoIcon: true
   },
   {
     id: '3',
-    title: 'Quarantine Pro app',
-    description: `Built using React Native and Expo, it's a small game that displays a fun message on how long you have been quarantining.`,
-    href: 'https://expo.io/@amanhimself/quarantinepro',
-    expoIcon: true
-  },
-  {
-    id: '4',
     title: '100DaysOfCode Twitter Bot',
     description: `Twitter bot for #100DaysOfCode community hosted by freeCodeCamp. Written in Node.js and currently hosted on a private repository.`,
     href: 'https://github.com/freeCodeCamp/100DaysOfCode-twitter-bot',
