7dc2f5324eeb86fbe0b62e92ae4a8202a2516804
Aman Mittal <amandeepmittal@live.com>
2024-07-18 16:20:56 +0530

    Sitemap updates

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,17 +1,14 @@
-import type { APIRoute } from "astro";
-import { SITE } from "@config";
+import type { APIRoute } from 'astro';
+import { SITE } from '@config';
 
 const robots = `
-User-agent: Googlebot
-Disallow: /nogooglebot/
-
 User-agent: *
 Allow: /
 
-Sitemap: ${new URL("sitemap-index.xml", SITE.website).href}
+Sitemap: ${new URL('/sitemap.xml', SITE.website).href}
 `.trim();
 
 export const GET: APIRoute = () =>
   new Response(robots, {
-    headers: { "Content-Type": "text/plain" },
+    headers: { 'Content-Type': 'text/plain' }
   });
