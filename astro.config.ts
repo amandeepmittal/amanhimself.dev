c16339441dd885022d4d21f3319dc177e01c884d
Aman Mittal <amandeepmittal@live.com>
2025-01-25 18:39:21 +0530

    Update code block theme and prettier formatting

--- astro.config.ts
+++ astro.config.ts
@@ -1,21 +1,21 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
-import remarkToc from "remark-toc";
-import remarkCollapse from "remark-collapse";
-import sitemap from "@astrojs/sitemap";
-import { SITE } from "./src/config";
-import { remarkReadingTime } from "./src/utils/remark-reading-time.mjs";
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+import remarkToc from 'remark-toc';
+import remarkCollapse from 'remark-collapse';
+import sitemap from '@astrojs/sitemap';
+import { SITE } from './src/config';
+import { remarkReadingTime } from './src/utils/remark-reading-time.mjs';
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
   integrations: [
     tailwind({
-      applyBaseStyles: false,
+      applyBaseStyles: false
     }),
     react(),
-    sitemap(),
+    sitemap()
   ],
   markdown: {
     remarkPlugins: [
@@ -24,20 +24,20 @@ export default defineConfig({
       [
         remarkCollapse,
         {
-          test: "Table of contents",
-        },
-      ],
+          test: 'Table of contents'
+        }
+      ]
     ],
     shikiConfig: {
-      theme: "catppuccin-latte",
+      theme: 'rose-pine-moon',
       wrap: true,
-      transformers: [],
-    },
+      transformers: []
+    }
   },
   vite: {
     optimizeDeps: {
-      exclude: ["@resvg/resvg-js"],
-    },
+      exclude: ['@resvg/resvg-js']
+    }
   },
-  scopedStyleStrategy: "where",
+  scopedStyleStrategy: 'where'
 });
