4261b4d04ca42233c093882778310728c7bb4152
Aman Mittal <amandeepmittal@live.com>
2026-01-04 18:36:47 +0530

    Change dark mode code block theme to Nord theme

--- astro.config.ts
+++ astro.config.ts
@@ -6,6 +6,7 @@ import remarkCollapse from 'remark-collapse';
 import sitemap from '@astrojs/sitemap';
 import { SITE } from './src/config';
 import { remarkReadingTime } from './src/utils/remark-reading-time.mjs';
+import nord from '@shikijs/themes/nord';
 
 // https://astro.build/config
 export default defineConfig({
@@ -31,7 +32,7 @@ export default defineConfig({
     shikiConfig: {
       themes: {
         light: 'github-light',
-        dark: 'github-dark'
+        dark: nord
       },
       wrap: true,
       transformers: []
